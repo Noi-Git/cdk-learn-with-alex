a1c073d98214f62eb7bf9a0701c360b4d917e931
Noi-Git <noi.patsin@gmail.com>
2025-02-01 20:30:36 -0500

    create a photos stack basic skeleton

--- bin/cdk-1.ts
+++ bin/cdk-1.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib'
 import { Cdk1Stack } from '../lib/cdk-1-stack'
+import { PhotosStack } from '../lib/PhotosStack'
 
 const app = new cdk.App()
-new Cdk1Stack(app, 'Cdk1Stack', {})
+// new Cdk1Stack(app, 'Cdk1Stack', {})
+new PhotosStack(app, 'PhotosStack')
