d2af744375b86d3f6027e84f6815c70fb8373628
Noi-Git <noi.patsin@gmail.com>
2025-02-01 19:10:57 -0500

    create 3 type of constructor to create bucket with cdk

--- lib/cdk-1-stack.ts
+++ lib/cdk-1-stack.ts
@@ -1,9 +1,49 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3'
+
+// create L3 level constructor
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id)
+
+    new Bucket(this, 'L3Bucket', {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(expiration),
+        },
+      ],
+    })
+  }
+}
 
 export class Cdk1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    // create L1 constructor
+    new CfnBucket(this, 'myL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [
+          {
+            expirationInDays: 1,
+            status: 'Enabled',
+          },
+        ],
+      },
+    })
+
+    // create L2 level constructor
+    new Bucket(this, 'MyL2Bucket', {
+      //expired based on time and auto-delete
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(2),
+        },
+      ],
+    })
+
+    // call L3 level constructor
+    new L3Bucket(this, 'MyL3Bucket', 3)
   }
 }
