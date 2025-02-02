0c03b7eaf5e765501e111483e8f0bf45807feb60
Noi-Git <noi.patsin@gmail.com>
2025-02-01 21:44:13 -0500

    call PhotosHandlerStack

--- bin/cdk-1.ts
+++ bin/cdk-1.ts
@@ -2,7 +2,9 @@
 import * as cdk from 'aws-cdk-lib'
 import { Cdk1Stack } from '../lib/cdk-1-stack'
 import { PhotosStack } from '../lib/PhotosStack'
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack'
 
 const app = new cdk.App()
 // new Cdk1Stack(app, 'Cdk1Stack', {})
 new PhotosStack(app, 'PhotosStack')
+new PhotosHandlerStack(app, 'PhotosHandlerStack')
