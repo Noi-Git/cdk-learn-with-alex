e20e2a45e1bf7bfd0d835cb0ca074ba82b986684
Noi-Git <noi.patsin@gmail.com>
2025-02-01 20:54:56 -0500

    create a private property that contain value of the stack id

--- lib/PhotosStack.ts
+++ lib/PhotosStack.ts
@@ -3,9 +3,18 @@ import { Bucket } from 'aws-cdk-lib/aws-s3'
 import { Construct } from 'constructs'
 
 export class PhotosStack extends cdk.Stack {
+  private stackSuffix: string
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
     new Bucket(this, 'PhotosBucket')
+  }
+
+  private initializeSufix() {
+    //grab data in the index 2
+    const shortStackId = cdk.Fn.select(2, cdk.Fn.split('/', this.stackId))
+    //grab data in the index 4
+    this.stackSuffix = cdk.Fn.select(4, cdk.Fn.split('-', this.stackId))
   }
 }
